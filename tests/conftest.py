1e2a13f8cd0ffab72aa464b345da61ed5434aaf9
Oleg Sher <admin@sher.biz>
2022-10-16 18:01:57 +0300

    fixed test_server dependency error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
 import pytest
 import logging as logger
 import requests
-from constants import api
+from constants import api, server_ip, server_port
 
 
 @pytest.fixture()
-def is_server_up(server_ip, server_port):
+def is_server_up():
     logger.info("Checking server")
     if requests.get(f'http://{server_ip}:{server_port}/api/{api}').status_code == 200:
         logger.info(f"Server is reachable, response code 200")
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,5 +1,5 @@
 import json
-
+import logging as logger
 import requests
 import pytest
 
@@ -8,13 +8,24 @@ from constants import server_ip, server_port, api
 
 # @pytest.mark.skip
 def test_get_positive(is_server_up):
+    """
+        Check response when server reachable, could be parametrized
+    """
+    logger.info(test_get_positive.__doc__)
     res = requests.get(f'http://{server_ip}:{server_port}/api/{api}')
     res_json = json.loads(res.content)
+
     assert res.status_code == 200 and res_json['version'] == 0.1 and res_json['name'] == 'salt'
 
 
 @pytest.mark.xfail
-def test_get_negative(is_server_up):
+def test_get_negative():
+    """
+    Check response when server down/unreachable, in this case I just point test to server_port + 1, no needs for
+    teardown or setup
+    """
+    logger.info(test_get_negative.__doc__)
     res = requests.get(f'http://{server_ip}:{server_port + 1}/api/{api}')
     res_json = json.loads(res.content)
+
     assert res.status_code == 200 and res_json['version'] == 0.1 and res_json['name'] == 'salt'
