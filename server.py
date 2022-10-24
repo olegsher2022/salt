bec3166b257997a283c42bfa43c9512b5099d1ef
Oleg Sher <admin@sher.biz>
2022-10-24 23:34:58 +0300

    added stock api last day

--- server.py
+++ server.py
@@ -3,24 +3,28 @@ from flask_restful import Api, Resource
 from constants import server_ip, server_port
 import pandas as pd
 
+
 # data = pd.read_csv('math_students.csv', delimiter=',')
 # data_apple = pd.read_csv('datasets//s&p500//AAPL.csv')
 
 
 class Main(Resource):
-    def get(self, company):
+    def get(self, company, date):
         if company == "0":
             data = pd.read_csv('math_students.csv', delimiter=',')
-            return data.to_json(orient="records")
+            return data.head().to_json(orient="records")
         else:
-            data = pd.read_csv(f'datasets//s&p500//{company}.csv', delimiter='\\')
-            return data.to_json(orient="records")
-
+            if date == "last":
+                data = pd.read_csv(f'datasets//s&p500//{company}.csv', delimiter='\\')
+                return data.tail(1).to_json(orient="records")
+            else:
+                data = pd.read_csv(f'datasets//s&p500//{company}.csv', delimiter='\\')
+                return data.to_json(orient="records")
 
 
 app = Flask(__name__)
 api = Api()
-api.add_resource(Main, "/api/info/<company>")
+api.add_resource(Main, "/api/info/<company>/<date>")
 api.init_app(app)
 
 if __name__ == '__main__':
