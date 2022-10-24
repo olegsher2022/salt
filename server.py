e7c560eda871f8ce8c8a86d487c51ea438d3af98
Oleg Sher <admin@sher.biz>
2022-10-24 23:21:51 +0300

    added stock api

--- server.py
+++ server.py
@@ -3,17 +3,24 @@ from flask_restful import Api, Resource
 from constants import server_ip, server_port
 import pandas as pd
 
-data = pd.read_csv('math_students.csv', delimiter=',')
+# data = pd.read_csv('math_students.csv', delimiter=',')
+# data_apple = pd.read_csv('datasets//s&p500//AAPL.csv')
 
 
 class Main(Resource):
-    def get(self):
-        return data.to_json(orient="records")
+    def get(self, company):
+        if company == "0":
+            data = pd.read_csv('math_students.csv', delimiter=',')
+            return data.to_json(orient="records")
+        else:
+            data = pd.read_csv(f'datasets//s&p500//{company}.csv', delimiter='\\')
+            return data.to_json(orient="records")
+
 
 
 app = Flask(__name__)
 api = Api()
-api.add_resource(Main, "/api/info")
+api.add_resource(Main, "/api/info/<company>")
 api.init_app(app)
 
 if __name__ == '__main__':
